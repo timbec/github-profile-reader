3f555ad98457b425c63583fbe96e28d529540c68
Tim Beckett <timbeckett@ymail.com>
2019-06-05 11:54:41 -0400

    Can now search for users on API.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component, Fragment } from 'react';
+
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search'; 
+
 import axios from 'axios';
 import './App.css';
 
@@ -12,7 +15,7 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+
     this.setState({ loading: true });
 
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={process.env.REACT_APP_GITHUB_SECRET_ID}`);
@@ -20,11 +23,26 @@ class App extends Component {
     console.log(res.data);
 
     this.setState({ users: res.data, loading: false });
-
-
   }
 
+  //Search Github Users
+  searchUsers = async text => {
+
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={process.env.REACT_APP_GITHUB_SECRET_ID}`);
+
+    console.log(res.data);
+
+    this.setState({ users: res.data.items, loading: false });
+  }
+
+  // Clear users from state: 
+  clearUsers = () => this.setState({ users: [], loading: false }); 
+
   render() {
+    const { users, loading} = this.state; 
+
     return (
       <Fragment>
         <header className="App-header">
@@ -34,7 +52,13 @@ class App extends Component {
           />
         </header>
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search 
+          searchUsers={this.searchUsers} clearUsers={this.clearUsers}
+          showClear={
+            users.length > 0 ? true : false
+          }
+           />
+          <Users loading={loading} users={this.state.users} />
         </div>
       </Fragment>
     );
