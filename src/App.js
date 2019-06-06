6067df32a708f5c13cf9e835389ae8010bdf7509
Tim Beckett <timbeckett@ymail.com>
2019-06-06 07:40:10 -0400

    Finished first section. App more or less complete. Now for refactoring.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   }
 
   async componentDidMount() {
@@ -55,6 +56,17 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   }
 
+  // Get a single Github user 
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={process.env.REACT_APP_GITHUB_SECRET_ID}`);
+
+    console.log('repos data: ' + res.data);
+
+    this.setState({ repos: res.data, loading: false });
+  }
+
 
   // Clear users from state: 
   clearUsers = () => this.setState({ users: [], loading: false });
@@ -67,7 +79,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -104,7 +116,12 @@ class App extends Component {
                 <User
                   {...props}
                   getUser={this.getUser}
-                  user={user} loading={loading} />
+                  getUserRepos={this.getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading}
+
+                />
               )} />
 
           </Switch>
